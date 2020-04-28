ec5bbed1df9ca33f69919e3b3d4a65df429a5a1e
Gretakoutika <53313650+Gretakoutika@users.noreply.github.com>
2020-04-28 16:35:04 +0530

    Create equi.java

---
+++ equi.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+class Persons {
+	String name;
+	float amount;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public float getAmount() {
+		return amount;
+	}
+	public void setAmount(float amount) {
+		this.amount = amount;
+	}
+	//float amount;
+	Persons(String name,float amount)
+	{
+		this.amount=amount;
+		this.name=name;
+	}
+	public String toString()
+	{
+		return getName() + "have to pay"+getAmount();
+	}
+	public Persons() {
+		// TODO Auto-generated constructor stub
+	}
+}
+public class equsplit {
+	public static void main(String args[])
+	{
+		float amount;
+		System.out.println("Enter the amount:");
+		Scanner sc=new Scanner(System.in);
+		amount=sc.nextFloat();
+		System.out.println("enter the number of persons:");
+		 // Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		  Persons p[]=new Persons[n];
+		  for(int i=0;i<n;i++)
+		  {
+			  System.out.println("enter your Name:");
+			  String s=sc.nextLine();
+			   p[i]=new Persons(s,amount/n);
+			  }
+		  for(int i=0;i<n;i++)
+			{
+				System.out.println(p[i]);
+			}  
+		 
+	}
+	
+
+}
